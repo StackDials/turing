2304b6f70d564e48e34bafcb99eaa200686bdfcc
nguyenhainam24082000 <nghainam24082000>
2023-01-16 18:03:50 +0700

    Add isOdd

---
+++ src/isOdd/isOdd.ts
@@ -0,0 +1,17 @@
+import { isNumber } from "../isNumber";
+
+function isOdd(value: any): boolean {
+  const n = Math.abs(value);
+  if (!isNumber(n)) {
+    throw new TypeError("expected a number");
+  }
+  if (!Number.isInteger(n)) {
+    throw new Error("expected an integer");
+  }
+  if (!Number.isSafeInteger(n)) {
+    throw new Error("value exceeds maximum safe integer");
+  }
+  return n % 2 === 1;
+}
+
+export default isOdd;
